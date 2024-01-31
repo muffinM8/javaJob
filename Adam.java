a1113340ccdc064a0007fdc444e02e31e0e4e57e
muffinM8 <ax2adam@gmail.com>
2024-01-31 20:41:39 +0200

    Change output once again.

--- Adam.java
+++ Adam.java
@@ -1,7 +1,8 @@
 public class Adam{
     public static void main(String[] args){
         for(int i =1; i<=10;i++){
-            System.out.println("I'm Adam :D "+i);
+            System.out.println("This is actually working! "+i);
         }
+        System.out.println("Yay!");
     }
 }
